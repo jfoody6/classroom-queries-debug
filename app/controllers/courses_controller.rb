c054f38c8ebaf6bf85769dc47e0b2e1f7879c197
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-05-01 21:39:33 +0000

    courses done

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -13,37 +13,41 @@ class CoursesController < ApplicationController
   end
 
   def create
-    @course = Course.new
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_")
-    @course.department_id = params.fetch("query_department_id")
+    course = Course.new
+    course.title = params.fetch("query_title")
+    course.term_offered = params.fetch("query_term")
+    course.department_id = params.fetch("query_department_id")
 
-    if @course.valid?
-      @course.save
+    if course.valid?
       redirect_to("/courses", { :notice => "Course created successfully." })
     else
       redirect_to("/courses", { :notice => "Course failed to create successfully." })
     end
+    course.save
+
   end
 
+
   def update
-    id = params.fetch("path_id")
-    @course = Course.where({ :id => the_id }).at(0)
+    the_id = params.fetch("path_id")
+    matching_records = Course.where({ :id => the_id })
+    the_course = matching_records.at(0)
 
-    @course.title = params.fetch("query_title")
-    @course.term_offered = params.fetch("query_term_offered")
-    @course.department_id = params.fetch("query_department_id")
+    the_course.title = params.fetch("query_title")
+    the_course.term_offered = params.fetch("query_term_offered")
+    the_course.department_id = params.fetch("query_department_id")
 
-    if @course.valid?
-      @course.save
-      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
+    if the_course.valid?
+      the_course.save
+      redirect_to("/courses/#{the_course.id}", { :notice => "Course updated successfully."} )
     else
-      redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
+      redirect_to("/courses/#{the_course.id}", { :alert => "Course failed to update successfully." })
     end
   end
 
-  def destroy
-    the_id = params.fetch("path")
+
+def destroy
+    the_id = params.fetch("path_id")
     @course = Course.where({ :id => the_id }).at(0)
 
     @course.destroy
